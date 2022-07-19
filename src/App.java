77584dc2f994e9ccaf18495eabf847cc4f32a408
Yam-BS <yam_b777@outlook.com>
2022-07-18 22:40:16 -0300

    Estilizando a saída

--- src/App.java
+++ src/App.java
@@ -21,11 +21,13 @@ public class App {
         var parser = new JsonParser();
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
+        System.out.println("\u001b[1mTop 250 filmes IMDb\u001b[m \n");
+
         // exibir e manipular os dados
         for (Map<String, String> filme : listaDeFilmes) {
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
+            System.out.println("\u001b[1mTítulo \u001b[m: \u001b[3m " + filme.get("title") + " " + "\u001b[m\uD83C\uDF7F");
+            System.out.println("\u001b[1mCapa\u001b[m: " + filme.get("image"));
+            System.out.println("\u001b[1mAvaliação\u001b[m: " + filme.get("imDbRating")  + "\u2B50");
             System.out.println();
         }
 
