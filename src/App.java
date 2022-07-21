f22b79207b9fd35ebea8e783a7d8d275eed1d25f
Yam-BS <yam_b777@outlook.com>
2022-07-21 07:22:13 -0300

    codigo refatorado

--- src/App.java
+++ src/App.java
@@ -1,10 +1,5 @@
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 
@@ -13,35 +8,37 @@ public class App {
 
         // fazer uma conexão HTTP e buscar os top 250 filmes
         String url = "https://api.mocki.io/v2/549a5d8b";
-        URI endereco = URI.create(url);
-        var client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder(endereco).GET().build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String body = response.body();
+        ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDb();
 
-        // pegar só os dados  que interessam (titulo, poster, classificação)
-        var parser = new JsonParser();
-        List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
-        System.out.println("\u001b[1mTop 250 filmes IMDb\u001b[m \n");
+//        String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
+//        ExtratorDeConteudo extrator = new ExtratorDeConteudoDaNasa();
+
+        var http = new ClienteHttp();
+        String json = http.buscaDados(url);
 
         // exibir e manipular os dados
+
+        List<Conteudo> conteudos = extrator.extraiConteudos(json);
+
         var geradora = new GeradoraDeFigurinhas();
-        for (Map<String, String> filme : listaDeFilmes) {
 
-            String urlImagem = filme.get("image");
-            String titulo = filme.get("title");
+        for (int i = 0; i < 3; i++) {
 
-            InputStream inputStream = new URL(urlImagem).openStream();
-            String nomeArquivo = titulo + ".png";
+            Conteudo conteudo = conteudos.get(i);
+
+            InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
+            String nomeArquivo = "saida/" + conteudo.getTitulo() + ".png";
 
             geradora.cria(inputStream, nomeArquivo);
 
-            System.out.println("\u001b[1mTítulo\u001b[m: \u001b[3m" + titulo + " " + "\u001b[m\uD83C\uDF7F");
-//            System.out.println("\u001b[1mTítulo\u001b[m: \u001b[3m" + filme.get("title") + " " + "\u001b[m\uD83C\uDF7F");
-//            System.out.println("\u001b[1mCapa\u001b[m: " + filme.get("image"));
-//            System.out.println("\u001b[1mAvaliação\u001b[m: " + filme.get("imDbRating")  + "\u2B50");
-            System.out.println();
+            System.out.println(conteudo.getTitulo());
+
+//            System.out.println("\u001b[1mTítulo\u001b[m: \u001b[3m" + titulo + " " + "\u001b[m\uD83C\uDF7F");
+//            System.out.println("\u001b[1mTítulo\u001b[m: \u001b[3m" + conteudo.get("title") + " " + "\u001b[m\uD83C\uDF7F");
+//            System.out.println("\u001b[1mCapa\u001b[m: " + conteudo.get("image"));
+//            System.out.println("\u001b[1mAvaliação\u001b[m: " + conteudo.get("imDbRating")  + "\u2B50");
+//            System.out.println();
         }
     }
 }
